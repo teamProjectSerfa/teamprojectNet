da97d2f2fb97f81985cf819435eb764f6ababe4f
teamProjectSerfa <60262218+teamProjectSerfa@users.noreply.github.com>
2020-02-04 13:46:38 +0100

    my first commit

---
+++ assets/js/script.js
@@ -0,0 +1,46 @@
+$(document).scroll(function() {
+    var isScrolled = $(this).scrollTop() > $(".topBar").height();
+    $(".topBar").toggleClass("scrolled", isScrolled);
+})
+
+function volumeToggle(button) {
+    var muted = $(".previewVideo").prop("muted");
+    $(".previewVideo").prop("muted", !muted);
+
+    $(button).find("i").toggleClass("fa-volume-mute");
+    $(button).find("i").toggleClass("fa-volume-up");
+}
+
+function previewEnded() {
+    $(".previewVideo").toggle();
+    $(".previewImage").toggle();
+}
+
+function goBack() {
+    window.history.back();
+}
+
+function startHideTimer() {
+    var timeout = null;
+    
+    $(document).on("mousemove", function() {
+        clearTimeout(timeout);
+        $(".watchNav").fadeIn();
+
+        timeout = setTimeout(function() {
+            $(".watchNav").fadeOut();
+        }, 2000);
+    })
+}
+
+function restartVideo() {
+    $("video")[0].currentTime = 0;
+    $("video")[0].play();
+    $(".upNext").fadeOut();
+}
+function watchVideo(videoId) {
+    window.location.href = "watch.php?id=" + videoId;
+}
+function showUpNext() {
+    $(".upNext").fadeIn();
+}
---
+++ includes/classes/PreviewProvider.php
@@ -0,0 +1,101 @@
+<?php
+class PreviewProvider {
+
+    private $con, $username;
+
+    public function __construct($con, $username) {
+        $this->con = $con;
+        $this->username = $username;
+    }
+
+    public function createCategoryPreviewVideo($categoryId) {
+        $entitiesArray = EntityProvider::getEntities($this->con, $categoryId, 1);
+
+        if(sizeof($entitiesArray) == 0) {
+            ErrorMessage::show("No TV shows to display");
+        }
+
+        return $this->createPreviewVideo($entitiesArray[0]);
+    }
+
+   
+    public function createTVShowPreviewVideo() {
+        $entitiesArray = EntityProvider::getTVShowEntities($this->con, null, 1);
+
+        if(sizeof($entitiesArray) == 0) {
+            ErrorMessage::show("No TV shows to display");
+        }
+
+        return $this->createPreviewVideo($entitiesArray[0]);
+    }
+
+    public function createMoviesPreviewVideo() {
+        $entitiesArray = EntityProvider::getMoviesEntities($this->con, null, 1);
+
+        if(sizeof($entitiesArray) == 0) {
+            ErrorMessage::show("No movies to display");
+        }
+
+        return $this->createPreviewVideo($entitiesArray[0]);
+    }
+
+    public function createPreviewVideo($entity) {
+        
+        if($entity == null) {
+            $entity = $this->getRandomEntity();
+        }
+
+        $id = $entity->getId();
+        $name = $entity->getName();
+        $preview = $entity->getPreview();
+        $thumbnail = $entity->getThumbnail();
+
+        // TODO: ADD SUBTITLE
+        $videoId = VideoProvider::getEntityVideoForUser($this->con, $id);
+
+        return "<div class='previewContainer'>
+
+                    <img src='$thumbnail' class='previewImage' hidden>
+
+                    <video autoplay muted class='previewVideo' onended='previewEnded()'>
+                        <source src='$preview' type='video/mp4'>
+                    </video>
+
+                    <div class='previewOverlay'>
+                        
+                        <div class='mainDetails'>
+                            <h3>$name</h3>
+
+                            <div class='buttons'>
+                                <button onclick='watchVideo($videoId)'><i class='fas fa-play'></i> Play</button>
+                                <button onclick='volumeToggle(this)'><i class='fas fa-volume-mute'></i></button>
+                            </div>
+
+                        </div>
+
+                    </div>
+        
+                </div>";
+
+    }
+
+    public function createEntityPreviewSquare($entity) {
+        $id = $entity->getId();
+        $thumbnail = $entity->getThumbnail();
+        $name = $entity->getName();
+
+        return "<a href='entity.php?id=$id'>
+                    <div class='previewContainer small'>
+                        <img src='$thumbnail' title='$name'>
+                    </div>
+                </a>";
+    }
+
+    private function getRandomEntity() {
+
+        $entity = EntityProvider::getEntities($this->con, null, 1);
+        return $entity[0];
+    }
+
+}
+?>
